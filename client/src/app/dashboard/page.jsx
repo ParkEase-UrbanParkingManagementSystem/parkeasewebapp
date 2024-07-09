6f8a2d1dbb0d7235edaf240001a2bb5347334eb6
Pasindi Vindula <123557234+pasi2001@users.noreply.github.com>
2024-07-09 23:39:14 +0530

    charts added
    dashboard is almost done. can change value regards to the timeline

--- client/src/app/dashboard/page.jsx
+++ client/src/app/dashboard/page.jsx
@@ -1,103 +1,125 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Card from "../../ui/dashboard/card/card";
-import Chart from "../../ui/dashboard/chartPmcRevenue/Chart";
-import Dropdown from "../../ui/dashboard/dropdown/dropdown"
-import { faSquareParking, faPerson, faCar, faPercent } from "@fortawesome/free-solid-svg-icons";
+import BarChart from "../../ui/dashboard/chartPmcRevenue/barchart";
+import LineChart from "../../ui/dashboard/chartPmcRevenue/linechart";
+import Dropdown from "../../ui/dashboard/dropdown/dropdown";
+import {
+  faSquareParking,
+  faPerson,
+  faCar,
+  faPercent,
+  faHandHoldingDollar,
+} from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { useState } from "react";
 
 library.add(faSquareParking);
 library.add(faPerson);
 library.add(faCar);
 library.add(faPercent);
+library.add(faHandHoldingDollar);
 
 const Dashboard = ({ setAuth }) => {
-
-  const [timeFrame, setTimeFrame] = useState('Daily');
+  const [timeFrame, setTimeFrame] = useState("Daily");
 
   const data = {
     Daily: {
       cardItemswarden: [
-        { title: 'Total Registered Wardens', content: '60' },
-        { title: 'Active Wardens', content: '54' },
-        { title: 'New Registered Wardens', content: '2' }
+        { title: "Total Registered Wardens", content: "60" },
+        { title: "Active Wardens", content: "54" },
+        { title: "New Registered Wardens", content: "2" },
       ],
       cardItemsparking: [
-        { title: 'Total Parking Areas', content: '10' },
-        { title: 'New Parking Areas', content: '1' }
+        { title: "Total Parking Areas", content: "10" },
+        { title: "New Parking Areas", content: "1" },
       ],
       cardItemsslot: [
-        { title: 'Total Slots Available', content: '400' },
-        { title: 'Occupied Slots', content: '100' },
-        { title: 'Remaining Slots', content: '300' }
+        { title: "Total Slots Available", content: "400" },
+        { title: "Occupied Slots", content: "100" },
+        { title: "Remaining Slots", content: "300" },
       ],
       cardItemsrate: [
-        { title: 'Average Parking Duration', content: '1 hour' },
-        { title: 'Turnover Rate', content: '10%' }
+        { title: "Average Parking Duration", content: "1 hour" },
+        { title: "Turnover Rate", content: "10%" },
       ],
+      cardItemsrevenue: [{ title: "Total Revenue", content: "300,000" }],
+      chart1: <BarChart />,
+      chart2: <LineChart />,
     },
     Weekly: {
       cardItemswarden: [
-        { title: 'Total Registered Wardens', content: '60' },
-        { title: 'Active Wardens', content: '54' },
-        { title: 'New Registered Wardens', content: '6' }
+        { title: "Total Registered Wardens", content: "60" },
+        { title: "Active Wardens", content: "54" },
+        { title: "New Registered Wardens", content: "6" },
       ],
       cardItemsparking: [
-        { title: 'Total Parking Areas', content: '10' },
-        { title: 'New Parking Areas', content: '2' }
+        { title: "Total Parking Areas", content: "10" },
+        { title: "New Parking Areas", content: "2" },
       ],
       cardItemsslot: [
-        { title: 'Total Slots Available', content: '400' },
-        { title: 'Occupied Slots', content: '200' },
-        { title: 'Remaining Slots', content: '200' }
+        { title: "Total Slots Available", content: "400" },
+        { title: "Occupied Slots", content: "200" },
+        { title: "Remaining Slots", content: "200" },
       ],
       cardItemsrate: [
-        { title: 'Average Parking Duration', content: '1.5 hours' },
-        { title: 'Turnover Rate', content: '15%' }
+        { title: "Average Parking Duration", content: "1.5 hours" },
+        { title: "Turnover Rate", content: "15%" },
       ],
+      cardItemsrevenue: [{ title: "Total Revenue", content: "900,000" }],
+      chart1: <BarChart />,
+      chart2: <LineChart />,
     },
     Monthly: {
       cardItemswarden: [
-        { title: 'Total Registered Wardens', content: '60' },
-        { title: 'Active Wardens', content: '50' },
-        { title: 'New Registered Wardens', content: '10' }
+        { title: "Total Registered Wardens", content: "60" },
+        { title: "Active Wardens", content: "50" },
+        { title: "New Registered Wardens", content: "10" },
       ],
       cardItemsparking: [
-        { title: 'Total Parking Areas', content: '10' },
-        { title: 'New Parking Areas', content: '3' }
+        { title: "Total Parking Areas", content: "10" },
+        { title: "New Parking Areas", content: "3" },
       ],
       cardItemsslot: [
-        { title: 'Total Slots Available', content: '400' },
-        { title: 'Occupied Slots', content: '250' },
-        { title: 'Remaining Slots', content: '150' }
+        { title: "Total Slots Available", content: "400" },
+        { title: "Occupied Slots", content: "250" },
+        { title: "Remaining Slots", content: "150" },
       ],
       cardItemsrate: [
-        { title: 'Average Parking Duration', content: '2 hours' },
-        { title: 'Turnover Rate', content: '20%' }
+        { title: "Average Parking Duration", content: "2 hours" },
+        { title: "Turnover Rate", content: "20%" },
       ],
+      cardItemsrevenue: [{ title: "Total Revenue", content: "12,000,000" }],
+      chart1: <BarChart />,
+      chart2: <LineChart />,
     },
     Yearly: {
       cardItemswarden: [
-        { title: 'Total Registered Wardens', content: '60' },
-        { title: 'Active Wardens', content: '40' },
-        { title: 'New Registered Wardens', content: '20' }
+        { title: "Total Registered Wardens", content: "60" },
+        { title: "Active Wardens", content: "40" },
+        { title: "New Registered Wardens", content: "20" },
       ],
       cardItemsparking: [
-        { title: 'Total Parking Areas', content: '10' },
-        { title: 'New Parking Areas', content: '4' }
+        { title: "Total Parking Areas", content: "10" },
+        { title: "New Parking Areas", content: "4" },
       ],
       cardItemsslot: [
-        { title: 'Total Slots Available', content: '400' },
-        { title: 'Occupied Slots', content: '300' },
-        { title: 'Remaining Slots', content: '100' }
+        { title: "Total Slots Available", content: "400" },
+        { title: "Occupied Slots", content: "300" },
+        { title: "Remaining Slots", content: "100" },
       ],
       cardItemsrate: [
-        { title: 'Average Parking Duration', content: '3 hours' },
-        { title: 'Turnover Rate', content: '25%' }
+        { title: "Average Parking Duration", content: "3 hours" },
+        { title: "Turnover Rate", content: "25%" },
       ],
-    }
+      cardItemsrevenue: [{ title: "Total Revenue", content: "87,000,000" }],
+      chart1: <BarChart />,
+      chart2: <LineChart />,
+    },
+  };
+
+  const handleTimeFrameChange = (selectedTimeFrame) => {
+    setTimeFrame(selectedTimeFrame);
   };
 
   return (
@@ -105,33 +127,45 @@ const Dashboard = ({ setAuth }) => {
       <h1 className="text-2xl">Dashboard</h1>
 
       <div className="my-2">
-        <Dropdown 
-          options={['Daily', 'Weekly', 'Monthly', 'Yearly']} 
-          selectedOption={timeFrame} 
-          onChange={setTimeFrame} 
+        <Dropdown
+          options={["Daily", "Weekly", "Monthly", "Yearly"]}
+          selectedOption={timeFrame}
+          onChange={handleTimeFrameChange}
         />
       </div>
 
       <div className="flex flex-row mb-4">
-        <div className="w-1/4">
+        <div className="w-1/5">
           <Card items={data[timeFrame].cardItemsslot} icon={faCar} />
         </div>
 
-        <div className="w-1/4">
-          <Card items={data[timeFrame].cardItemsparking} icon={faSquareParking} />
+        <div className="w-1/5">
+          <Card
+            items={data[timeFrame].cardItemsparking}
+            icon={faSquareParking}
+          />
         </div>
 
-        <div className="w-1/4">
+        <div className="w-1/5">
           <Card items={data[timeFrame].cardItemswarden} icon={faPerson} />
         </div>
 
-        <div className="w-1/4">
+        <div className="w-1/5">
           <Card items={data[timeFrame].cardItemsrate} icon={faPercent} />
+        </div>
+
+        <div className="w-1/5">
+          <Card
+            items={data[timeFrame].cardItemsrevenue}
+            icon={faHandHoldingDollar}
+          />
         </div>
       </div>
 
-      <div className="flex mt-8 w-1/2">
-        <Chart />
+      <div className="flex flex-row mt-8 space-x-4">
+        <div className="w-1/2">{data[timeFrame].chart1}</div>
+
+        <div className="w-1/2">{data[timeFrame].chart2}</div>
       </div>
     </div>
   );
---
+++ client/src/ui/dashboard/chartPmcRevenue/linechart.jsx
@@ -0,0 +1,58 @@
+"use client"
+
+import { useRef, useEffect } from "react";
+import { Chart } from "chart.js/auto";
+import React from 'react';
+
+const LineChartPMC = () => {
+    const chartRef = useRef(null);
+
+    useEffect(() => {
+        if (chartRef.current) {
+            if (chartRef.current.chart) {
+                chartRef.current.chart.destroy();
+            }
+
+            const context = chartRef.current.getContext("2d");
+
+            const newChart = new Chart(context, {
+                type: "line", 
+                data: {
+                    labels: ["8.00", "9.00", "10.00", "11.00", "12.00",
+                                "13.00", "14.00", "15.00", "16.00", "17.00", "18.00", "19.00", "20.00"
+                    ],
+                    datasets: [{
+                        label: "Peak parking hours (#ofVehicles)",
+                        data: [34, 54, 65, 76, 83, 52, 43, 56, 21, 32, 9, 7,8],
+                        backgroundColor: "#ffb403",
+                        borderColor: "#000000",
+                        borderWidth: 1,
+                        fill: false, 
+                        tension: 0.1 
+                    }],
+                },
+                options: {
+                    responsive: true,
+                    scales: {
+                        x: {
+                            type: "category"
+                        },
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
+
+            chartRef.current.chart = newChart;
+        }
+    }, []); 
+
+    return (
+        <div style={{ position: "relative", width: "38vw", height: "20vw" }}>
+            <canvas ref={chartRef} />
+        </div>
+    );
+}
+
+export default LineChartPMC;
