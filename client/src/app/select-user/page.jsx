edecbc0e49642831de9bb71bfc404d2110fb60e4
Pasindi Vindula <123557234+pasi2001@users.noreply.github.com>
2024-07-20 02:59:05 +0530

    select user page done

--- client/src/app/select-user/page.jsx
+++ client/src/app/select-user/page.jsx
@@ -4,8 +4,14 @@ import styles from "./selectuser.module.css";
 function Users() {
   return (
     <div className={styles.container}>
-      <div>PMC</div>
-      <div>Driver</div>
+      <div className={styles.driver}>
+        <img src="images/driver user.png" className={styles.userpic} />
+        <h1 className={styles.title}>Driver</h1>
+      </div>
+      <div className={styles.pmc}>
+        <img src="images/office-building.png" className={styles.userpic} />
+        <h1 className={styles.title}>Parking Management Company</h1>
+      </div>
     </div>
   );
 }
