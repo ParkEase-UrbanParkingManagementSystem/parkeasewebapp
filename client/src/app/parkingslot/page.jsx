859c0584a860eb3cbc0064d7cd5d5ca83f44d08c
Nipul Yansith <nipulyansith26@gmail.com>
2024-07-24 18:36:29 +0530

    Update page.jsx

--- client/src/app/parkingslot/page.jsx
+++ client/src/app/parkingslot/page.jsx
@@ -97,11 +97,10 @@ const ParkingLot = () => {
                     <div className={styles.link}>{lot.name}</div>
                   </Link>
                 </td>
-                <td className={styles.empnamedata}>
+                {/* <td className={styles.empnamedata}>
                   {lot.fname} {lot.lname}
                 </td> */}
                 <td className={styles.empnamedata}>{lot.wardens ? "Assigned" : "Not Assigned"}</td>
-
                 <td className={styles.empgenderdata}>
                   <FontAwesomeIcon icon={faCar} className={styles.icon} />{" "}
                   {lot.car_capacity} &nbsp;
@@ -116,7 +115,7 @@ const ParkingLot = () => {
                   />{" "}
                   {lot.xlvehicle_capacity}
                 </td>
-                <td
+                {/* <td
                   className={
                     lot.status === "Active"
                       ? styles.statusActive
@@ -124,10 +123,8 @@ const ParkingLot = () => {
                   }
                 >
                   {lot.status}
-
                 </td> */}
                 <td className={`${styles.statusInactive} font-bold text-green-500`} >Active</td>
-
               </tr>
             ))}
           </tbody>
