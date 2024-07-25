b412679fa0463fd607823acac4738e67c010036d
cheya01 <2021cs206@stu.ucsc.cmb.ac.lk>
2024-07-25 09:01:43 +0530

    finish set up server and db

--- server/index.js
+++ server/index.js
@@ -1,27 +1,29 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const dotenv = require("dotenv");
 
+// Load environment variables from .env file
+dotenv.config();
 
-//middleware
-app.use(express.json()) //req.body
-app.use(cors())
+// Use the PORT variable from the .env file or default to 3000
+const port = process.env.PORT || 3000;
 
-//ROUTES
+// Middleware
+app.use(express.json()); //req.body
+app.use(cors());
 
-//register and login routes
-
+// Routes
 app.use("/auth", require("./routes/jwtAuth"));
 app.use("/wardens", require("./routes/wardens"));
 app.use("/pmc", require("./routes/pmcRoutes"));
-app.use("/parkinglots", require("./routes/parkingLotRoutes"))
+app.use("/parkinglots", require("./routes/parkingLotRoutes"));
 app.use("/driver", require('./routes/driverRoutes'));
-app.use("/vehicle",require('./routes/vehicleRoutes'));
+app.use("/vehicle", require('./routes/vehicleRoutes'));
 
-// //Dashboard route
-
+// // Dashboard route
 // app.use("/dashboard", require("./routes/dashboard"))
 
-app.listen(5000,()=>{
-    console.log("The server is running on port 5000");
-})
+app.listen(port, () => {
+    console.log(`The server is running on port ${port}`);
+});
