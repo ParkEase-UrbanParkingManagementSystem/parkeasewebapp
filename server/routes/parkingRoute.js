b9637a515b3886d2f3d97227359e6219323b0731
Nipul Yansith <nipulyansith26@gmail.com>
2024-08-24 01:44:21 +0530

    parkingControllerChanged

--- server/routes/parkingRoute.js
+++ server/routes/parkingRoute.js
@@ -6,6 +6,9 @@ const authorization = require("../middleware/authorization");
 
 router.get("/parking-details", authorization, parkingController.getParkingDetails);
 router.get("/parked-details", authorization, parkingController.getAfterParkingDetails);
+router.get("/parked-detailsMob", authorization, parkingController.getAfterParkingDetailsMobile);
+router.post("/pay-wallet", authorization, parkingController.payByWallet);
+router.post("/pay-pp", authorization, parkingController.payByParkPoints);
 
 
 module.exports = router;
