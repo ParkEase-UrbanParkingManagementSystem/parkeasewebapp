b258c3fea4cb300523cb4d98a9b40b759c55fc1e
Nipul Yansith <117194615+nipulyansith@users.noreply.github.com>
2024-09-06 23:17:46 +0530

    Merge pull request #91 from ParkEase-UrbanParkingManagementSystem/04-nipul
    04 nipul

--- server/routes/parkingRoute.js
+++ server/routes/parkingRoute.js
@@ -12,6 +12,8 @@ router.get("/get-recent-parking-lots-home", authorization, parkingController.get
 router.get("/get-instance-details/:id", authorization, parkingController.getParkingInstanceDetails);
 router.post("/pay-wallet", authorization, parkingController.payByWallet);
 router.post("/pay-pp", authorization, parkingController.payByParkPoints);
+router.post("/pay-cash", authorization, parkingController.payByCash);
+router.get("/get-parking-status", authorization, parkingController.checkDriverStatus)
 
 //HEllo
 
