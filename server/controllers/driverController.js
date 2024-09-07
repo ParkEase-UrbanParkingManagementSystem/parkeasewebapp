0f6f6c92cd182e7e6d75930f6c48618f6153137c
Nipul Yansith <nipulyansith26@gmail.com>
2024-09-08 02:12:17 +0530

    parkpoints retrieve

--- server/controllers/driverController.js
+++ server/controllers/driverController.js
@@ -42,3 +42,73 @@ exports.getDriverDetails = async (req, res) => {
         res.status(500).json({ msg: "Server Error" });
     }
 }
+
+exports.getWalletBalance = async (req, res) => {
+    try {
+      const user_id = req.user; // Ensure req.user contains the user ID
+  
+      const walletBalance = await pool.query(`
+        SELECT
+          p.available_amount
+        FROM 
+          payparkwallet p
+        JOIN
+          driver d ON p.driver_id = d.driver_id
+        JOIN
+          users u ON d.user_id = u.user_id
+        WHERE
+          u.user_id = $1
+      `, [user_id]);
+  
+      if (walletBalance.rows.length === 0) {
+        return res.status(404).json({ msg: "Wallet balance not found" });
+      }
+  
+      console.log(walletBalance.rows[0]);
+
+      res.status(200).json({
+        message: "Success",
+        data: walletBalance.rows[0]
+      });
+  
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).json({ msg: "Server Error" });
+    }
+  };
+
+
+  exports.getParkPointsBalance = async (req, res) => {
+    try {
+      const user_id = req.user; // Ensure req.user contains the user ID
+  
+      const pointsBalance = await pool.query(`
+        SELECT
+          p.no_of_points
+        FROM 
+          parkpoints p
+        JOIN
+          driver d ON p.driver_id = d.driver_id
+        JOIN
+          users u ON d.user_id = u.user_id
+        WHERE
+          u.user_id = $1
+      `, [user_id]);
+  
+      if (pointsBalance.rows.length === 0) {
+        return res.status(404).json({ msg: "PayPark Points balance not found" });
+      }
+  
+      console.log(pointsBalance.rows[0]);
+
+      res.status(200).json({
+        message: "Success",
+        data: pointsBalance.rows[0]
+      });
+  
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).json({ msg: "Server Error" });
+    }
+  };
+  
--- server/routes/driverRoutes.js
+++ server/routes/driverRoutes.js
@@ -5,5 +5,7 @@ const authorization = require('../middleware/authorization');
 const authorizeRole = require('../middleware/roleAuthorization');
 
 router.get('/details', authorization, authorizeRole([1]), driverController.getDriverDetails);
+router.get('/getWalletBalance', authorization, authorizeRole([1]), driverController.getWalletBalance);
+router.get('/getParkPoints', authorization, authorizeRole([1]), driverController.getParkPointsBalance);
 
 module.exports = router;
