f2e4054622c3d9b01209c71851c43140814b1924
dilki01 <dillenora@gmail.com>
2024-12-02 09:00:45 +0530

    create-checkout-session

--- client/src/pages/api/create-checkout-session.js
+++ client/src/pages/api/create-checkout-session.js
@@ -7,10 +7,12 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
 export default async function handler(req, res) {
     if (req.method === 'POST') {
         try {
+            console.log(req.headers.origin);
             const { amount } = req.body;
 
             // Create Checkout Sessions from body params.
             const session = await stripe.checkout.sessions.create({
+                
                 payment_method_types: ['card'],
                 line_items: [
                     {
@@ -25,9 +27,12 @@ export default async function handler(req, res) {
                     },
                 ],
                 mode: 'payment',
-                success_url: `${req.headers.origin}/wallet?session_id={CHECKOUT_SESSION_ID}`,
-                // success_url: `${req.headers.origin}/driver/wallet`,
-                cancel_url: `${req.headers.origin}/wallet`,
+                // console.log(req.headers.origin);
+                
+                // success_url: `${req.headers.origin}/driver/wallet?session_id={CHECKOUT_SESSION_ID}`,
+                // success_url: `${req.headers.origin}/wallet`,
+                success_url: `${req.headers.origin}/driver/wallet?session_id={CHECKOUT_SESSION_ID}`,
+                cancel_url: `${req.headers.origin}/driver/wallet`,
             });
 
             res.status(200).json({ url: session.url });
